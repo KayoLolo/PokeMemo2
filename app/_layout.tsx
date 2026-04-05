c5f8ef3bd9b08ec2ee9b7c6bec627839b6b9861f
bramandl <idarling.off@gmail.com>
2026-04-05 09:49:57 +0200

    feat(Screens): screen path management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="HomeScreen" options={{ title: 'HomeScreen' }} />
+      <Stack.Screen name="index" options={{ title: 'PokeMemo' }} />
+      <Stack.Screen name="details" options={{ title: 'Détails' }} />
     </Stack>
   );
 }
